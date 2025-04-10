d69f5b2f73ef5f9a4262b48121f76bb691726541
Alish313 <102430993+Alish1005@users.noreply.github.com>
2025-04-10 08:21:04 +0300

    moreedit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -22,7 +22,7 @@ function Sidebar() {
                     </li>
                     <li className='sidebarListItem'>
                         <span className='sidebarListItemText'>
-                            <ChatIcon className='sidebarIcon'/>Chats2
+                            <ChatIcon className='sidebarIcon'/>Chats
                         </span>
                     </li>
                     <li className='sidebarListItem'>
